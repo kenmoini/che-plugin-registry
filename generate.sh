e1caa61f04ef4f03b9b45ecec9548278dd62ea7e
Ken Moini <ken@kenmoini.com>
2025-03-03 04:23:05 +0000

    bug fixes and ordering

--- generate.sh
+++ generate.sh
@@ -16,6 +16,13 @@ python3 ${SCRIPT_DIR}/generate-mirror-json.py -o /tmp/che-plugin-registry/devspa
 
 cd /tmp/che-plugin-registry/devspaces/dependencies/che-plugin-registry/
 
+# Ignore prerelease exclusions causing failures to build - some extensions don't ever remove the pre-release flag
 sed -i 's|$preRelease == true|$preRelease == "nottrue"|g' build/scripts/download_vsix.sh
+
+# Stop buggy extensions with poor deps from failing 
+# - vscode.github-authentication
+# - redhat.vscode-yaml
+sed -i 's|set -e||g' build/scripts/import_vsix.sh
+sed -i 's|set -o pipefail||g' build/scripts/import_vsix.sh
 
 ./build.sh -t ${TAG} -o ${ORG} --offline && podman push quay.io/${ORG}/pluginregistry-rhel9:${TAG}
